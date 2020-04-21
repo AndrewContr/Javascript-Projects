7dc9aa65cbffc8dbd3a1b53ec4eb8edd7a53aafe
AndrewContr <61569744+AndrewContr@users.noreply.github.com>
2020-04-21 01:55:03 -0600

    Add files via upload

---
+++ Basic Javascript Projects/JS/Basic_Javascript_1.js
@@ -0,0 +1,21 @@
+document.write("Hello, World!");
+var A="This is a string";
+document.write(A);
+var X= "1979";
+window.alert(X);
+document.write("USA v. Beefsteak: \"But beefsteak isn\'t not delicious!\"");
+
+var S="\"If emptiness is loneliness," + " and loneliness is cleanliness," + " and cleanliness is godliness,"
++ " then God is empty, just like me.\"";
+document.write(S);
+
+var Family="A Team", Brains="Hannibal", Looks="Face", Muscle="Baracas", Wildcard="Murdock";
+document.write(Muscle);
+var Brains=Brains.fontcolor("gray");
+var Looks=Looks.fontcolor("green");
+var Muscle=Muscle.fontcolor("red");
+var Wildcard=Wildcard.fontcolor("purple");
+
+document.write(Wildcard);
+document.write(Muscle);
+document.write(6+9);
