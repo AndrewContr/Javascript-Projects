b85b6ec1933f9ea0544abfe05a47988972a6105a
AndrewContr <61569744+AndrewContr@users.noreply.github.com>
2020-05-14 20:05:10 -0600

    Add files via upload

---
+++ Basic Javascript Projects/JS/Basic_Javascript_2.js
@@ -0,0 +1,11 @@
+function YEEAHH() {
+    var str="Dark Side of the Thriller!";
+    var result=str.fontcolor("green");
+    document.getElementById("Green_Text").innerHTML=result;
+}
+
+function myFunction() {
+    var sentence="I am learning";
+    sentence +=" a lot from this book!";
+    document.getElementById("Concatenate").innerHTML=sentence;
+}
